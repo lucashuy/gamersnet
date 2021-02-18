5232e42a2f759ad792b9d060a5fd1699cf628a91
Lucas <contact@itslucas.win>
2021-02-18 11:40:32 -0800

    Added comments

--- gamersnet_backend/persistence/users.js
+++ gamersnet_backend/persistence/users.js
@@ -3,20 +3,28 @@
 let MongoDB = require('./mongodb');
 
 async function getUsers() {
+    // connect wait for server to connect to db
     let db = await MongoDB.open();
+
+    // once it connected, get the "users" collection (aka a table in SQL)
     let users = db.collection('users');
 
+    // wait for the server to find all users and return as an array
     let result = await users.find({});
     return result.toArray();;
 }
 
 async function addUser(username) {
+    // wait for db connection and get users collection
     let db = await MongoDB.open();
     let users = db.collection('users');
 
+    // insert one row into the database, where the username key is our parameter
     users.insertOne({username: username});
 
+    // return something...
     return {status: 200};
 }
 
+// make these two functions "public" to the rest of the project
 module.exports = {getUsers, addUser};
