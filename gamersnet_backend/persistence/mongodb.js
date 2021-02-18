2491162b4c1d1325eddd2b4867a920c01c2e577f
Lucas <contact@itslucas.win>
2021-02-18 11:40:32 -0800

    Added comments

--- gamersnet_backend/persistence/mongodb.js
+++ gamersnet_backend/persistence/mongodb.js
@@ -6,11 +6,18 @@ require('dotenv').config();
 
 class MongoDB {
     static async open() {
+        // this is a singleton, we dont want to flood the server with lots of open connections
+        // if we already connected to the database, just return that connection
         if (this.db) return this.db;
         
+        // create our url
+        // this is in format of: mongodb://username:password@server_ip:port/database_name
         let url = 'mongodb://' + process.env.MONGODB_USER + ':' + process.env.MONGODB_PWRD + '@' + process.env.MONGODB_HOST + ':' + process.env.MONGODB_PORT + '/' + process.env.MONGODB_DB;
+
+        // wait for the server to connect to the database
         this.__client = await mongodb.connect(url, {useUnifiedTopology: true})
 
+        // get the database from the client and return it
         this.db = this.__client.db(process.env.MONGODB_DB);
         return this.db
     }
--- gamersnet_backend/routes/users/index.js
+++ gamersnet_backend/routes/users/index.js
@@ -1,9 +1,12 @@
 let app = require('express').Router();
 
+// include each route handler
 let createAccount = require('./createAccount');
 let getAllUsers = require('./getAllUsers');
 
+// specify the routes under /users/ and pass them off to each function
 app.post('/createAccount', createAccount)
 app.post('/getAllUsers', getAllUsers)
 
+// return the above routes
 module.exports = app;
