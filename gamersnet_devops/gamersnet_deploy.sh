da3b1210ffe1e86362e2166449a2b647cdcf5b5b
Lucas <contact@itslucas.win>
2021-02-18 08:33:01 -0800

    Included install commands in deploy

--- gamersnet_devops/gamersnet_deploy.sh
+++ gamersnet_devops/gamersnet_deploy.sh
@@ -5,5 +5,8 @@ set -e
 cd ./gamersnet
 git pull origin master
 cd ./gamersnet_frontend
+npm install
 npm run build
-sudo systemctl restart gamersnet_backend.service
+cd ../gamersnet_backend
+npm install
+sudo systemctl restart gamersnet-backend.service
