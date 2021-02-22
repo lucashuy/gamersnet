9d8c63d739c50ab3ea70f3214e4dbbb6dd48ff7b
Lucas <contact@itslucas.win>
2021-02-21 16:44:36 -0800

    Added comments

--- gamersnet_backend/routes/users/authenticate.js
+++ gamersnet_backend/routes/users/authenticate.js
@@ -21,20 +21,24 @@ function verifyPasswordRequirements(password) {
 async function authenticate(request, response) {
     let body = request.body;
 
+    // validate if username and password exist
     let validUsername = verifyUsernameRequirements(body.username);
     let validPassword = verifyPasswordRequirements(body.password);
 
     if (validUsername && validPassword) {
+        // search db for document containing username
         let result = await getUserByUsername(body.username)
         let correctPassword = false;
 
+        // additionally check if the given password matches the database hash
         if (result) await bcrypt.compare(body.password, result.password).then((correct) => correctPassword = correct);
 
         if (correctPassword) {
+            // make a new token and update it
             let tokenNew = await generateToken(result._id);
-
             await updateUserToken(result._id, tokenNew);
 
+            // give client token
             response.cookie('token', tokenNew, {maxAge: TOKEN_LIFE_SPAN, httpOnly: true});
             response.status(204).end();
         } else {
