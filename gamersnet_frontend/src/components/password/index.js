094a56f34c81594c7b27693a5ffa6bdad88a6720
Lucas <contact@itslucas.win>
2021-02-24 11:16:11 -0800

    Added basic styles to password change

--- gamersnet_frontend/src/components/password/index.js
+++ gamersnet_frontend/src/components/password/index.js
@@ -1,24 +1,52 @@
 import React from 'react';
 import APIFetch from '../../api';
 
+import './styles.css';
+
 export default class Password extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = {data: ''}
+        this.state = {newPassword: '', message: ''};
+
+        this.inputPassword = this.inputPassword.bind(this);
+        this.handle = this.handle.bind(this);
     }
 
-    componentDidMount() {
-        let fetchData = APIFetch('page2');
-        fetchData.then((data) => {
-            this.setState({data: JSON.stringify(data)});
-        });
+    inputPassword(event) {
+        this.setState({newPassword: event.target.value});
+    }
+
+    handle(event) {
+        this.setState({message: 'sending, pls wait'});
+
+        if (this.state.newPassword !== '') {
+            let body = {password: this.state.newPassword};
+
+            let fetchData = APIFetch('/users/changePassword', JSON.stringify(body), 'POST');
+
+            fetchData.then(async (data) => {
+                if (await data.ok) {
+                    this.props.history.push('/');
+                    this.props.updateHeader();
+                } else {
+                    this.setState({message: 'something went wrong'});
+                }
+            });
+        }
+
+        event.preventDefault();
     }
 
     render() {
         return (
-            <div>
-                <p>this is page 2 and the server says: {this.state.data}</p>
+            <div className = 'login-form'>
+                <p>Change Password</p>
+                <form onSubmit = {this.handle} autoComplete = 'off' className = 'vertical-center'>
+                    <input type = 'password' onChange = {this.inputPassword} placeholder = 'new password' />
+                    <button onClick = {this.handle}>press me</button>
+                </form>
+                <p>{this.state.message}</p>
             </div>
         );
     }
