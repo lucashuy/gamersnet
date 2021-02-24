2eb2e523da680cd7f3d90fb7c558d8bb8228b921
Lucas <contact@itslucas.win>
2021-02-23 16:21:18 -0800

    Added CORS required headers

--- gamersnet_backend/app.js
+++ gamersnet_backend/app.js
@@ -12,6 +12,8 @@ app.use((request, response, next) => {
     // we have to set CORS to let our frontend access the backend
     // our frontend is (currently) set to port 3001 while this backend is on 3000
     response.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
+    response.setHeader('Access-Control-Allow-Methods', '*');
+    response.setHeader('Access-control-Allow-Headers', '*');
 
     next();
 });
