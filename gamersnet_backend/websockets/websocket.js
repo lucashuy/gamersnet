6f877710e57a92f850dea7f8b955f3de1f80ee6c
JayKhakhariya <jaykhakhariya40@gmail.com>
2021-03-24 20:23:10 +0530

    chat messages receive from web sockets initial setup

--- gamersnet_backend/websockets/websocket.js
+++ gamersnet_backend/websockets/websocket.js
@@ -1,20 +1,65 @@
-function disconnected(){
-    console.log("client has disconnected");
-}
+let clients = []
+
+
 
 function messageReceived(message){
     // console.log("Received: " + message);
 
     var myMessage = JSON.parse(message);
     console.log(myMessage)
+
+    if(myMessage.type == "name"){
+        clients.push(myMessage.data)
+        console.log(clients)
+    }
 }
 
 function webSocketOnConnect(websocket){
     console.log("client connected")
+    
 
-    websocket.on("message", messageReceived);
+    websocket.on("message", function(message){
+        // console.log("Received: " + message);
 
-    websocket.on("close", disconnected)
+        var myMessage = JSON.parse(message);
+        console.log(myMessage)
+
+        // user sends a receiver userID everytime a web socket is connected
+        if(myMessage.type == "userID"){
+
+            // store the client and it's corresponding userID in order to send the message
+            // inefficient but works for now
+            clients.push({client : websocket, userID : myMessage.userID})
+        }
+        else if(myMessage.type == "message"){
+
+            // message contains who to send the message to (receiver's userID)
+            // only send it to the receiver; not anyone else (one-to-one chat)
+            var receiver = clients.find(function (receiver) { return receiver.userID === myMessage.receiver; });
+
+            // client socket that sent the message
+            var sender = clients.find(function (sender) { return sender.client === websocket; });
+
+            console.log(receiver.userID)
+            console.log(sender.userID)
+            
+
+            // also if the client is not online or even not connected to the socket, don't need to send anything
+            // we can have chat notification systen for users when they are not online
+            // send the message to the receiver and pass in the userID of the sender
+            receiver.client.send(JSON.stringify({
+                userID: sender.userID,
+                message: myMessage.message
+            }));
+        }
+
+    });
+
+    websocket.on("close", function(){
+        console.log("client has disconnected");
+        var clientToBeDeleted = clients.find(function (sender) { return sender.client === websocket; });
+        delete clientToBeDeleted;
+    })
 }
 
-module.exports = {webSocketOnConnect};
+module.exports = { webSocketOnConnect };
